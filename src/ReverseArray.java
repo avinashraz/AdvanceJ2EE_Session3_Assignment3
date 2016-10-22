f9eda1a9698628e6b80ec2af201e9c24e3deb5f7
avinashraz <avinashaninthaneni@gmail.com>
2016-10-22 23:40:19 +0530

    Reverse Array

---
+++ src/ReverseArray.java
@@ -0,0 +1,13 @@
+
+public class ReverseArray {
+
+	public static void main(String[] args)
+	{
+	int []number={99,55,88,75,22,44,10,20,78};
+	for(int i = number.length-1; i >=0; --i)
+{
+	System.out.println(number[i]);	
+}
+	}
+
+}
